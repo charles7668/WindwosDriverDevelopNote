2e205c445b40684d5e45b8af9a983d678ef7334e
charles <charles76680119@gmail.com>
2023-09-17 00:21:29 +0800

    加入 簡易設備物件的驅動範例 的應用程式專案

---
+++ DeviceObjectExample/DeviceObjectExampleConsoleApp/DeviceObjectExampleConsoleApp.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+#include <Windows.h>
+
+int main()
+{
+	// create device object file
+	const HANDLE handle = CreateFile(TEXT("\\\\.\\DeviceObjectExample"),
+	                                 GENERIC_READ | GENERIC_WRITE | GENERIC_EXECUTE,
+	                                 FILE_SHARE_READ | FILE_SHARE_WRITE,
+	                                 nullptr,
+	                                 OPEN_EXISTING,
+	                                 FILE_ATTRIBUTE_NORMAL, nullptr);
+	if (handle == INVALID_HANDLE_VALUE)
+	{
+		std::cout << "CreateFile failed with error code " << GetLastError() << std::endl;
+		return -1;
+	}
+	std::cout << "CreateFile success" << std::endl;
+	std::cin.get();
+	return 0;
+}
